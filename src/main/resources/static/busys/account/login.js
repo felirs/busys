e7e05f8fa602d56b81b272cc187563b525bd00c4
郭显阳 <peflapos.guo@ants-city.com>
2018-02-21 22:10:50 +0800

    弹窗提示修改

--- src/main/resources/static/busys/account/login.js
+++ src/main/resources/static/busys/account/login.js
@@ -1,26 +1,49 @@
-$(function(){
+$(function () {
+
 
     $('#w').window('open');
     var $btn_submit = $('a#login-btn-submit'),
         $btn_clear = $('a#login-btn-clear'),
         $form = $('#login-form');
 
-    $btn_submit.on('click',function () {
-        console.log($form.serialize());
+    $btn_submit.on('click', function () {
         $.ajax({
-            url:'/login',
-            method:'POST',
-            data:$form.serialize(),
-            success:function (result) {
+            url: '/login',
+            method: 'POST',
+            data: $form.serialize(),
+            success: function (result) {
                 var resultObj = JSON.parse(result);
-                if(resultObj.success){
-                    window.location.href="/index";
-                }else{
-                    alert('登录失败');
+                if (resultObj.success) {
+                    $.messager.show({
+                        title:'提示',
+                        msg: '登录成功!',
+                        timeout: 2000,
+                        style: {
+                            align: 'center'
+                        },
+                        showType: 'fade'
+                    });
+                    window.location.href = "/index";
+                } else {
+                    $.messager.show({
+                        title:'提示',
+                        msg: '登录失败',
+                        timeout: 2000,
+                        style: {
+                            align: 'center'
+                        },
+                        showType: 'fade'
+                    });
                 }
             },
-            error:function(){
-                alert('登录失败');
+            error: function () {
+                $.messager.show({
+                    title:'提示',
+                    msg: '登录失败',
+                    timeout: 2000,
+                    style: 'text-align:center;',
+                    showType: 'fade'
+                });
             }
         });
     });
